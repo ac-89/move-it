187ede6661f2396e6d7b11fad2b9323c50f9d630
ac-89 <alancarroll89@gmail.com>
2024-02-03 12:57:27 +0000

    Remove ejs

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const cloudinary = require("cloudinary");
+
+// cloudinary.config({
+//   cloud_name: "dcmlljv4l",
+//   api_key: "388785151925177",
+//   api_secret: "K0p7OZb5HjZ8NwZ9S6jJ0uqKuXs",
+//   secure: true,
+// });
 
 app.use(express.static(__dirname + "/public"));
-app.use(express.static(__dirname + "/images"));
 app.use(express.json());
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
+app.set("view engine", "html");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  // res.render("index");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 app.get("/galleryupload", (req, res) => {
-  res.render("upload");
+  // res.render("upload");
+  res.sendFile(__dirname + "/views/upload.html");
 });
 
 app.listen(process.env.PORT || PORT, () => {
